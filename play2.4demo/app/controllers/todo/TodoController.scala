b80f481670f8110eda77a83e7537a35010eac7c7
saito400 <ken.s.530000@gmail.com>
2015-06-28 16:07:28 +0900

    c

--- play2.4demo/app/controllers/todo/TodoController.scala
+++ play2.4demo/app/controllers/todo/TodoController.scala
@@ -7,6 +7,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class TodoController extends Controller {
 
+//  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+//  import driver.api._
+
   def index = Action.async { implicit request =>
     Future(Ok(views.html.todo.index()))
   }
