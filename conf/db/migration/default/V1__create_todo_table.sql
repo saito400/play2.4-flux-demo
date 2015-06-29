00f20b1343b9cce58e94db3a9fb96408a5506732
saito400 <ken.s.530000@gmail.com>
2015-06-29 21:39:21 +0900

    c

--- conf/db/migration/default/V1__create_todo_table.sql
+++ conf/db/migration/default/V1__create_todo_table.sql
@@ -1,11 +1,22 @@
 
-DROP TABLE IF EXISTS todo;
-
-CREATE TABLE todo
+drop table if exists todo_type;
+create table todo_type
 (
-    id int NOT NULL AUTO_INCREMENT,
-    content varchar(255) NOT NULL,
-     INS_DATETIME datetime NOT NULL,
-     UPD_DATETIME datetime NOT NULL,
-     PRIMARY KEY (id)
+    id int not null auto_increment,
+    title varchar(255) not null,
+    ins_datetime datetime not null,
+    upd_datetime datetime not null,
+    primary key (id)
 );
+
+drop table if exists todo;
+create table todo
+(
+    id int not null auto_increment,
+    todo_type_id int,
+    content varchar(255) not null,
+    ins_datetime datetime not null,
+    upd_datetime datetime not null,
+    primary key (id)
+);
+
