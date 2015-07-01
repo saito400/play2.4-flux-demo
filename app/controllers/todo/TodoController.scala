9e3f5ef1cfeb4ad33f582b287b324c71306c28c4
saito400 <ken.s.530000@gmail.com>
2015-07-01 21:33:28 +0900

    c

--- app/controllers/todo/TodoController.scala
+++ app/controllers/todo/TodoController.scala
@@ -1,17 +1,31 @@
 package controllers.todo
 
-import play.api._
-import play.api.mvc._
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.Play
+import play.api.data.Form
+import play.api.data.Forms.mapping
+import play.api.data.Forms.text
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.db.slick.HasDatabaseConfig
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc.Action
+import play.api.mvc.Controller
+import slick.driver.JdbcProfile
+import models.Tables
+import models.Tables._
 
-class TodoController extends Controller {
 
-//  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
-//  import driver.api._
+//class TodoController extends Controller with Tables with HasDatabaseConfig[JdbcProfile]{
+class TodoController extends Controller with HasDatabaseConfig[JdbcProfile]{
 
-  def index = Action.async { implicit request =>
-    Future(Ok(views.html.todo.index()))
+  val dbConfig = DatabaseConfigProvider.get[JdbcProfile](Play.current)
+  import driver.api._
+
+//  case class Todo(id: Int, todoTypeId: Option[Int] = None, content: String)
+
+  val Todos = TableQuery[models.Tables.Todo]
+
+  def index = Action.async {
+    db.run(Todos.result).map(res => Ok(views.html.todo.index(res.toList)))
   }
 
 }
